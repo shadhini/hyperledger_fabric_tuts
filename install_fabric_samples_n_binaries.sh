c792c2d9329d8ac8f64f8fb5c312cb234caa20de
shadhini <jshadhiniaseka@gmail.com>
2020-01-14 11:18:57 +0530

    install fabric samples and binaries updated

--- install_fabric_samples_n_binaries.sh
+++ install_fabric_samples_n_binaries.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env bash
 
-##### deprecated ########
-## Determine a location on your machine where you want to place the fabric-samples repository and
-## enter that directory in a terminal window
-#cd /home/shadhini/dev/repos/hyperledger_fabric
-## this script will download and extract all of the platform-specific binaries you will need to set up your network and
-## place them into the cloned repo you created above.
-## Finally, the script will download the Hyperledger Fabric docker images from Docker Hub into your local Docker registry and tag them as ‘latest’.
-#curl -sSL http://bit.ly/2ysbOFE | bash -s
-#
-## export PATH=<path to download location>/bin:$PATH
-#export PATH=/home/shadhini/dev/repos/hyperledger_fabric/fabric-samples/bin:$PATH
-
-##### end of deprecated ########
-
-
 env | grep GO
 # GOPATH=/home/shadhini/work
 
@@ -22,4 +7,11 @@ env | grep GO
 mkdir -p $GOPATH/src/github.com/hyperledger/
 cd $GOPATH/src/github.com/hyperledger/
 
-git clone https://github.com/hyperledger/fabric-samples.git
+# this script will download and extract all of the platform-specific binaries you will need to set up your network and
+# place them into the cloned repo you created above.
+# Finally, the script will download the Hyperledger Fabric docker images from Docker Hub into your local Docker registry and tag them as ‘latest’.
+curl -sSL http://bit.ly/2ysbOFE | bash -s
+#git clone https://github.com/hyperledger/fabric-samples.git
+
+# export PATH=<path to download location>/bin:$PATH
+export PATH=/home/shadhini/dev/repos/hyperledger_fabric/fabric-samples/bin:$PATH
